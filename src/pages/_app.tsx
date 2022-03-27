e2bb771b5a3b477b23786f3b498d0570c765b10a
Isaac Lopes <isaaclopinho@gmail.com>
2022-03-27 19:20:19 -0300

    fixed document title

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,10 +4,14 @@ import '../styles/theme.scss';
 import 'react-toastify/dist/ReactToastify.css';
 import type { AppProps } from 'next/app';
 import { ToastContainer } from 'react-toastify';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        <title>Did you know?</title>
+      </Head>
       <Component {...pageProps} />
       <ToastContainer />
     </>
