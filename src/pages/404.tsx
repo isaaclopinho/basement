5a161a03cc3323fba04e37a8e19be3098342a8ed
Isaac Lopes <isaaclopinho@gmail.com>
2022-03-27 21:33:35 -0300

    404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,7 +1,16 @@
+import Layout from 'components/templates/layout';
 import React from 'react';
+import styles from 'styles/404.module.scss';
 
 function Error() {
-  return <p> An error 404 occurred on server</p>;
+  return (
+    <Layout>
+      <div className={styles.container}>
+        <div className={styles.font}>404</div>
+        <h1>Desculpe, esta página não existe!</h1>
+      </div>
+    </Layout>
+  );
 }
 
 export default Error;
